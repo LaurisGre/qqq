d257dd86b7f187412b708b4bf20e70af3f5e3f75
LaurynasG <glaurynasg@gmail.com>
2020-10-20 16:07:41 +0300

    ele_bill

--- index.php
+++ index.php
@@ -1,51 +1,33 @@
 <?php
-    $child_count = rand(3, 8);
-    $child_price_per_drink = 0.65;
-    $child_total_drinks = 0;
-    $child_total_price = 0;
+    $days_in_month = 31;
+    $random_day = rand(1, 31);
+    $price_per_kwh = 0.07;
+    $total_kwh_used = 0;
 
-    $adult_count = rand(4, 18);
-    $adult_price_per_drink = 1.15;
-    $adult_total_drinks = 0;
-    $adult_total_price = 0;
-
-    $total_drinks = 0;
-    $total_price = 0;
-    
-    for ($i = 0; $i < $child_count; $i++) {
-        $drink_per_child = rand(1, 5);
-        $child_total_drinks += $drink_per_child;
-        $total_drinks += $drink_per_child;
-        $child_total_price += $drink_per_child * $child_price_per_drink;
-        $total_price += $drink_per_child * $child_price_per_drink;
+    for($i = 0; $i < $days_in_month; $i++){
+        $used_ped_day = rand(2, 5);
+        $total_kwh_used += $used_ped_day;
+        print "$i";
+        print "<br />";
+        if($i === $random_day) {
+            break;
+        }
     }
 
-    for ($u = 0; $u < $child_count; $u++) {
-        $drink_per_adult = rand(4, 8);
-        $adult_total_drinks +=  $drink_per_adult;
-        $total_drinks += $drink_per_adult;
-        $adult_total_price += $drink_per_adult * $adult_price_per_drink;
-        $total_price += $drink_per_adult * $adult_price_per_drink;
-    }
-
-    $depozito = $total_drinks * 0.1;
-
-    print "$child_count vaikai išgerė $child_total_drinks sulčių butelius už $child_total_price$";
+    $total_price = $total_kwh_used * $price_per_kwh;
+    print "Iki $random_day mėnesio dienos bus saunaudota $total_kwh_used kwh";
     print "<br />";
-    print "$adult_count dėdžių išgerė $adult_total_drinks alaus butelius už $adult_total_price$";
-    print "<br />";
-    print "Iš viso buvo išgertą $total_drinks buteliai už $total_price$ depozitas $depozito$";
+    print "Reiks sumokėti $$total_price";
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Electricity billt</title>
     <style>
     </style>
 </head>
 <body>
-    <h1>Drink Calculator</h1>
-
 </body>
 </html>
