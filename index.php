4717ad10bac803d9699b1c0e9b644a97beb21df8
LaurynasG <glaurynasg@gmail.com>
2020-10-21 16:34:37 +0300

    schwolt

--- index.php
+++ index.php
@@ -1,40 +1,119 @@
 <?php
-    $work_time = 12;
-    $profit = 0;
-    $people_spin = 0;
-    $people_left = 0;
-    $total_spins = 0;
+    $schwolt = [
+        [
+            'name' => 'Vilnius',
+            [
+                [
+                    'name' => 'shopname1',
+                    'work_hours' => [10, 22],
+                    'details' => [
+                        'take_out' => true,
+                        'min_order_price' => 10,
+                        'delivery_fee' => 1,
+                    ],
+                    'menu' => [
+                        'main_meanl' => [
+                            'kebab',
+                            'kotlet',
+                        ],
+                        'snacks' => [
+                            'fries',
+                            'crisps',
+                        ],
+                        'drinks' => [
+                            'beer',
+                            'kvass',
+                        ],
+                    ]
+                ],
+                [
+                    'name' => 'shopname2',
+                    'work_hours' => [10, 22],
+                    'details' => [
+                        'take_out' => true,
+                        'min_order_price' => 10,
+                        'delivery_fee' => 1,
+                    ],
+                    'menu' => [
+                        'main_meanl' => [
+                            'kebab',
+                            'kotlet',
+                        ],
+                        'snacks' => [
+                            'fries',
+                            'crisps',
+                        ],
+                        'drinks' => [
+                            'beer',
+                            'kvass',
+                        ],
+                    ],
+                ],
+            ],
+        ],
+        [
+            'name' => 'Klaipeda',
+            [
+                [
+                    'name' => 'shopname3',
+                    'work_hours' => [10, 22],
+                    'details' => [
+                        'take_out' => true,
+                        'min_order_price' => 10,
+                        'delivery_fee' => 1,
+                    ],
+                    'menu' => [
+                        'main_meanl' => [
+                            'kebab',
+                            'kotlet',
+                        ],
+                        'snacks' => [
+                            'fries',
+                            'crisps',
+                        ],
+                        'drinks' => [
+                            'beer',
+                            'kvass',
+                        ],
+                    ],
+                ],
+                [
+                    'name' => 'shopname4',
+                    'work_hours' => [10, 22],
+                    'details' => [
+                        'take_out' => true,
+                        'min_order_price' => 10,
+                        'delivery_fee' => 1,
+                    ],
+                    'menu' => [
+                        'main_meanl' => [
+                            'kebab',
+                            'kotlet',
+                        ],
+                        'snacks' => [
+                            'fries',
+                            'crisps',
+                        ],
+                        'drinks' => [
+                            'beer',
+                            'kvass',
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ];
 
-    for ($i = 0; $i < 12; $i++) {
-        $people_per_hour = rand(1, 20);
-        if ($people_per_hour >= 10) {
-            $total_spins++;
-            $profit += 130;
-            $people_spin += 10;
-            $people_left += $people_per_hour - 10;
-        } else {
-            $people_left += $people_per_hour;
-        }
-    }
-
-    $p1 = "Karusele uzdirbo $profit jevru";
-    $p2 = "Prasisuko $people_spin zmones";
-    $p3 = "Isejo $people_left zmogai";
-    $p4 = "Pilnu pakrovimu karusele sukosi $total_spins";
-
+    var_dump($schwolt);
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Carusele</title>
+    <title>Schwolt/title>
     <style>
     </style>
 </head>
 <body>
-    <p><?php print $p1; ?></p>
-    <p><?php print $p2; ?></p>
-    <p><?php print $p3; ?></p>
-    <p><?php print $p4; ?></p>
 </body>
 </html>
