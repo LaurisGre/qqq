9d7574a7c93633fb4814e321a9e1b6ee32e335af
LaurisDesktop <glaurynasg@gmail.com>
2020-11-03 15:56:49 +0200

    Ez tuesday

--- index.php
+++ index.php
@@ -1,72 +1,36 @@
 <?php
 
-//KLIUCIU RUOZAS
-$hurdle_jump =  [
-    rand(1, 10),
-    rand(1, 10),
-    rand(1, 10),
-    rand(1, 10),
-    rand(1, 10)
-];
+$number_array = [838, 121, 344, 555, 768, 878, 987, 345, 565];
 
-$jump_height = rand(5, 12);
-
-function check_jump($hurdle, $jump) {
-    foreach($hurdle as $height) {
-        if ($jump < $height) {
-            return false;
-        }
-    }
-    return true;
-}
-
-var_dump(check_jump($hurdle_jump, $jump_height));
-
-//LARGEST SWAP
-$two_digit_num = rand(10, 99);
-
-function check_swap($num) {
-    $num_arr = str_split($num);
-    $num_swapped = $num_arr[1] . $num_arr[0];
-    if ($num_swapped > $num) {
-        return true;
-    }
-
-    return false;
-}
-
-var_dump(check_swap($two_digit_num));
-
-//SORT ARRAY (USING REFERENCE)
-$array = [80, 29, 4, -95, -24, 85, 1, 2, 10, 50, 5];
-
-function sorter(&$arr) {
-    sort($arr);
-}
-
-sorter($array);
-var_dump($array);
-
-//ELIMINATE ODD NUMBERS (USING REFERENCE)
-$array2 = [
-    rand(1, 3000),
-    rand(1, 3000),
-    rand(1, 3000),
-    rand(1, 3000),
-    rand(1, 3000),
-    rand(1, 3000),
-];
-
-function odd_out(&$arr) {
-    foreach($arr as $key => $num) {
-        if (!($num % 2 === 0)) {
-            unset($arr[$key]);
+function sort_pal(&$arr) {
+    foreach($arr as $index => &$number) {
+        if (!(substr($number, 0, 1) === substr($number, -1, 1)) ) {
+            array_splice($arr, $index, 1);
         }
     }
 }
 
-odd_out($array2);
-var_dump($array2);
+sort_pal($number_array);
+var_dump($number_array);
+
+////
+
+$string = 'mano batai buvo du';
+
+function spongemock(&$str) {
+    $upper = true;
+    for ($i = 0 ; $i < strlen($str) ; $i++) {
+        if ($str[$i] != ' ') {
+            $upper ?
+            $str[$i] = strtoupper($str[$i]):
+            $str[$i] = strtolower($str[$i]);
+            $upper = !$upper;
+        }
+    }
+}
+
+spongemock($string);
+var_dump($string);
 
 ?>
 <!DOCTYPE html>
@@ -75,7 +39,7 @@ var_dump($array2);
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="style.css">
-    <title>EZ Friday</title>
+    <title>EZ Tuesday</title>
 </head>
 
 <body>
