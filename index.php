240f4e803a96e5108b1ba4dc4d91d36110c60479
LaurisDesktop <glaurynasg@gmail.com>
2020-11-04 15:49:39 +0200

    army 2

--- index.php
+++ index.php
@@ -1,21 +1,12 @@
 <?php
 
-$report = 'Choose your weather';
+$report = 'Please enter your personal details';
 
 if (isset($_POST['sub'])) {
-	switch ($_POST['weather']) {
-		case 'rainy':
-			$report = 'It will be rainy';
-			break;
-		case 'sunny':
-			$report = 'It will be sunny';
-			break;
-		case 'snowy':
-			$report = 'It will be snowy';
-			break;
-		case 'cloudy':
-			$report = 'It will be cloudy';
-			break;
+	if ($_POST['age'] < 18 || $_POST['age'] > 26 || $_POST['criminal'] === 'yes') {
+		$report = 'You\'re not going to the army';
+	} else {
+		$report = 'Congratulations you\'re going to the army';
 	}
 }
 
@@ -31,15 +22,14 @@ if (isset($_POST['sub'])) {
 
 <body>
 	<section>
-		<p>Select the weather type:</p>
 		<form method="POST">
-			<select name="weather" id="">
-				<option value="rainy">rainy</option>
-				<option value="sunny">sunny</option>
-				<option value="snowy">snowy</option>
-				<option value="cloudy">cloudy</option>
-			</select>
-			<input type="submit" name="sub">
+			<p>Provide your age:</p>
+			<input type="number" name="age" required>
+			<p>I am a criminal</p>
+			<input type="radio" name="criminal" value="yes" checked>
+			<p>I am not a crook</p>
+			<input type="radio" name="criminal" value="no">
+			<input type="submit" name="sub" value="Send">
 		</form>
 		<p><?php print $report; ?></p>
 	</section>
