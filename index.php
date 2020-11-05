10a2ac2a5de88a31c0ac7ecdc014f95431b90a06
LaurisDesktop <glaurynasg@gmail.com>
2020-11-05 16:36:18 +0200

    foooooorms

--- index.php
+++ index.php
@@ -1,39 +1,89 @@
 <?php
-$report = '';
+$message_sent = false;
+$data = [];
+
+function prep_data(array $data): array
+{
+	$preped_arr = [];
+	foreach ($data as $index => $value) {
+		if ($value !== 'Send') {
+			$preped_arr[] = "$index: $value";
+		}
+	}
+
+	return $preped_arr;
+}
+
+function validate_field_not_empty(string $value): bool
+{
+	if ($value === '') {
+		return false;
+	}
+	return true;
+}
 
 if (isset($_POST['sub'])) {
-	$trimmed_name = trim($_POST['name'], ' ');
-	if (strpos($trimmed_name, ' ')) {
-		$names = explode(' ', $_POST['name']);
-		$names_capitalize = [];
-		foreach ($names as $name) {
-			$names_capitalize[] = ucwords($name);
-		}
-		$name_string = implode(' ', $names_capitalize);
-		$report = $name_string;
-	} else {
-		$report = 'Please enter your name with spaces in between each name';
-	}
+	$message_sent = true;
+	$data = prep_data($_POST);
 }
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
 	<meta charset="UTF-8">
 	<link rel="stylesheet" href="style.css">
-	<title>Wednesday Forms</title>
+	<link href="https://fonts.googleapis.com/css2?family=Rye&display=swap" rel="stylesheet">
+	<title>Thurday Forms</title>
 </head>
 
 <body>
-	<section>
-		<form method="POST">
-			<input type="text" name="name">
-			<input type="submit" name="sub" value="Check">
-		</form>
-		<p><?php print $report; ?></p>
-	</section>
+	<?php if (!$message_sent) : ?>
+		<article class="message_send">
+			<header>WRITTE US A MESSAGE</header>
+			<form method="POST">
+				<input type="text" name="Name" placeholder="Your name" required>
+				<section class="split">
+					<input type="email" name="eMail" placeholder="eMail address" required>
+					<input type="number" name="Phone" placeholder="Phone number">
+				</section>
+				<p>I would like to contact:</p>
+				<label>
+					<input type="radio" name="Contact" value="Sales Department" required>
+					<p>Sales Department</p>
+				</label>
+				<label>
+					<input type="radio" name="Contact" value="Administration" required>
+					<p>Administration</p>
+				</label>
+				<label>
+					<input type="radio" name="Contact" value="Client Management" required>
+					<p>Client Management</p>
+				</label>
+				<select name="Subject" id="">
+					<option value="complaint">Complaint</option>
+					<option value="suggestion">Suggestion</option>
+					<option value="question">Question</option>
+				</select>
+				<textarea name="Message" placeholder="Your message here"></textarea>
+				<input type="submit" name="sub" value="Send" class="butt">
+			</form>
+		</article>
+	<?php else : ?>
+		<article class="message_info">
+			<header>
+				<p>Thank you for your message, <?php print $_POST['Name']; ?></p>
+				<section>
+					<p>Your message info:</p>
+					<?php foreach ($data as $value) : ?>
+						<p><?php print $value; ?></p>
+					<?php endforeach; ?>
+				</section>
+			</header>
+		</article>
+	<?php endif; ?>
 </body>
 
 </html>
